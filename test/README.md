139253c13c66bd6f56621d8980f8c6ecda3bb6b9
kee_joo (uidc0082) <kee.joo.yeo@continental-corporation.com>
2024-04-29 10:34:06 +0800

    Add test fixtures and text color

--- test/README.md
+++ test/README.md
@@ -5,6 +5,8 @@
 - https://www.browserstack.com/guide/unit-testing-python
 
 ## Execute Unit Test Case
+- Execute the following command in the same folder as the test file
 ```
+$python -m unittest <test_file.py>
 $python -m unittest -v <test_file.py>
 ```
--- test/test_calculator_smoke.py
+++ test/test_calculator_smoke.py
@@ -10,20 +10,32 @@ from source.calculator import button_click, clear_display, backspace, toggle_sig
 # import Python unittest
 import unittest
 
+
+# color text
+C_RED   = "\033[1;31m"
+C_GREEN = "\033[1;32m"
+C_YELLOW  = "\033[1;33m"
+C_BLUE    = "\033[1;34m"
+C_WHTIE = "\033[1;0m"
+C_RESET = C_WHTIE
+
 class TestCalculator(unittest.TestCase):
 
+
+    # execute on each test method
     def setUp(self):
+        print(f"{C_BLUE}\n[setup] clear display{C_RESET}")
         clear_display()
 
+
+    # execute on each test method
     def tearDown(self):
-        clear_display()
+        print(f"{C_BLUE}[teardown] do nothing{C_RESET}")
+
 
     def test_button_click(self):
         # Test button click functionality
         # Add your test cases here
-
-        # initialize
-        clear_display()
 
         # positive test cases
         # Number 0..9 button press
@@ -38,7 +50,6 @@ class TestCalculator(unittest.TestCase):
         # Test clear display functionality
         # Add your test cases here
 
-        clear_display()
         self.assertEqual(get_entry(), '')
 
 
@@ -46,9 +57,6 @@ class TestCalculator(unittest.TestCase):
         # Test backspace functionality
         # Add your test cases here
 
-        # initialize
-        clear_display()
-        
         # perform backspace when entry is empty
         backspace()
         self.assertNotEqual(get_entry(), 'Error')
@@ -68,18 +76,18 @@ class TestCalculator(unittest.TestCase):
         # Add your test cases here
 
         # perform negate when entry is empty
-        clear_display()
         toggle_sign()
         self.assertEqual(get_entry(), '-')
+        clear_display()
 
         # perform negate when entry contains a number
-        clear_display()
         button_click('12')
         toggle_sign()
         self.assertEqual(get_entry(), '-12')
 
         toggle_sign()
         self.assertEqual(get_entry(), '12')
+
 
     def test_operate(self):
         # Test arithmetic operation functionality
@@ -94,7 +102,6 @@ class TestCalculator(unittest.TestCase):
         # subtract a negative number from a negative number
 
         # multiply two positive numbers
-        clear_display()
         button_click('8*2')
         operate()
         self.assertEqual(get_entry(), '16')
@@ -107,6 +114,7 @@ class TestCalculator(unittest.TestCase):
         # divide two negative numbers
 
         # divide by zero error
+
 
 if __name__ == '__main__':
     unittest.main()
